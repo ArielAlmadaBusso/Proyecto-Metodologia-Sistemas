37f396a11ec2e1dcdb8e9468c96377644b24e968
Ariel Almada-Busso <cocoalmada56@gmail.com>
2023-09-25 22:30:19 -0300

    Creacion de visitor-seeder

---
+++ server/src/application/seeders/visitor-seeder.ts
@@ -0,0 +1,22 @@
+import  Visitor  from '../../domain/entities/visitor';
+import visitorRepository from '../../infrastructure/repositories/visitor_repository';
+
+class VisitorSeeder {
+  private visitors: Array<Visitor> = [];
+  
+  public constructor() {
+    this.visitors.push(Visitor.create('1','Maty'));
+    this.visitors.push(Visitor.create('2','Agus'));
+    this.visitors.push(Visitor.create('3','Romi'));
+    this.visitors.push(Visitor.create('4','Nadin'));
+    this.visitors.push(Visitor.create('5','Agustin'));
+  }
+
+  public async save(): Promise<void> {
+    for (const visitor of this.visitors) {
+      await visitorRepository.save(visitor);
+    }
+  }
+}
+
+export default new VisitorSeeder();
