69705714b1b046100d9487875f89e91d5e19bfb4
Matias Rubiolo <rubiolomatias2001@gmail.com>
2023-10-17 01:27:19 -0300

    feat:Added categoryRoutes

---
+++ server/src/http/routes/category.routes.ts
@@ -0,0 +1,17 @@
+import { Application } from 'express'
+import CommonRoutes from './common.routes'
+import GetCategoriesAction from 'http/actions/GetCategoriesAction'
+
+class CategoryRoutes extends CommonRoutes {
+  public constructor (app: Application) {
+    super(app, 'Category')
+  }
+
+  public setUpRoutes (): Application {
+    this.app.get('/category', GetCategoriesAction.run)
+
+    return this.app
+  }
+}
+
+export default CategoryRoutes
