9d7f2d0caa10afefe13d0669b81339b78400e436
bahiasolmunoz[200~ <bahiasolmunoz@gmail.com>
2023-11-03 22:23:16 -0300

    Correccion de codigo

--- server/src/application/commands/likeCommand.ts
+++ server/src/application/commands/likeCommand.ts
@@ -1,19 +1,27 @@
 class LikeCommand {
-  private readonly ip;
-  private readonly nickname;
-
-  public constructor(ip: string, nickname: string) {
-      
-      this.ip = ip;
-      this.nickname = nickname;
+    private readonly ip;
+    private readonly nickname;
+    private readonly pin: string;
+  
+    public constructor(ip: string, nickname: string, pin: string) {
+        
+        this.ip = ip;
+        this.nickname = nickname;
+        this.pin = pin;
+        
+    }
+    
+    public getIp(): string {
+        return this.ip;
+    }
+    public getNickname(): string {
+        return this.nickname;
+    }
+    public getPin(): string {
+      return this.pin;
   }
   
-  public getIp(): string {
-      return this.ip;
   }
-  public getNickname(): string {
-      return this.nickname;
-  }
-}
-
-export default LikeCommand;
+  
+  export default LikeCommand;
+  
